641dadc6e469a4905d418a2287a10f1dbaa142ad
edwinalegre <edwin_alegre@yahoo.com>
2016-07-18 15:58:59 -0700

    Authenticate and no dupes

--- server.js
+++ server.js
@@ -3,40 +3,31 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var database;
-var Message = mongoose.model('Message',{
-   msg: String
-});
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
-app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
-});
+})
 
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
-app.post('/api/message', function(req, res){
-    console.log(req.body);
-    var message = new Message(req.body);
-    message.save();
-    res.status(200);
-});
+app.post('/api/message', message.post);
 
-function GetMessages(req, res){
-    Message.find({}).exec(function(err, result){
-       res.send(result);
-    });
-}
+app.post('/auth/register', auth.register);
 
-mongoose.connect('mongodb://localhost:27017/test', function(err, db){
-   if(!err){
-       console.log('we connected to mongo');
-   }
-});
 
-var server = app.listen(5000, function(){
-    console.log('listening on port', server.address().port)
-});
+
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
+        console.log("we are connected to mongo");
+    }
+})
+
+var server = app.listen(5000, function () {
+    console.log('listening on port ', server.address().port)
+})
