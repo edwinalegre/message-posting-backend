34731f748c6961c29c2332e33cbf50ab2edc0c38
edwinalegre <edwin_alegre@yahoo.com>
2016-07-18 23:16:04 -0700

    Associate user with post

--- server.js
+++ server.js
@@ -5,23 +5,21 @@ var mongoose = require('mongoose');
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated')
+var cors = require('./services/cors')
 
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-})
-
+//Requrests
 app.get('/api/message', message.get);
 
-app.post('/api/message', message.post);
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
-
-
+//Connection
 mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
     if (!err) {
         console.log("we are connected to mongo");
